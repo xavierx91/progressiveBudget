8ec0f7581a37ea7a782948bdc6b50462bcb7f269
Xavier Martinez <xavier@Xaviers-MacBook-Air.local>
2021-06-03 17:15:22 -0500

    fix atlas

--- server.js
+++ server.js
@@ -16,11 +16,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-}).catch(error => error)
-
+mongoose.connect(
+     process.env.MONGODB_URI || 'mongodb://localhost/budget',
+     {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
+     }
+   );
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 })
